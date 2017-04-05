6242a9905fc41f4d663dded64e648d2f73cd2903
Dilunika <kasun.dilunika@contactenergy.co.nz>
2017-04-05 13:09:09 +1200

    Included bootstrap.min.css

--- src/components/display/DisplayScreen.js
+++ src/components/display/DisplayScreen.js
@@ -1,12 +1,13 @@
 import React, {PropTypes} from 'react';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import './display.css';
 
 const DisplayScreen = ({text}) => {
 
     return (
-        <div className="screen">
+        <Jumbotron className="screen">
             {text}
-        </div>
+        </Jumbotron>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// Demo component
-// this is only example component
-// you can find tests in __test__ folder
-
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import './bootstrap.min.css';
 import './index.css';
 import DisplayScreen from './components/display/DisplayScreen';
 
@@ -23,7 +21,7 @@ class RandomNumberDisplay extends React.Component {
         return (
             <div className="container">
                 <DisplayScreen text={this.state.randomNumber + ''}/>
-                <button onClick={() => this.generateRandomNumber()} type="button">Generate Random Number</button>
+                <Button onClick={() => this.generateRandomNumber()} bsStyle="primary">Generate Random Number</Button>
             </div>
         )
     }
